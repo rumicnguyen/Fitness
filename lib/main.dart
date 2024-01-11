ef81c5145d4b3d68624e7f81d663cb1d42e84264
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-11 23:46:56 +0700

    feat: Firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitness_app/firebase_options/firebase_options.dart';
 import 'package:fitness_app/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
 
-void main() {
-  runApp(
-    const MyApp(),
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  
+  await dot_env.dotenv.load(
+    fileName: "assets/.env",
+  );
+
+  runApp(const MyApp());
 }
