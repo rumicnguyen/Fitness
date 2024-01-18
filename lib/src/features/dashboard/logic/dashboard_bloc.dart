055803d75e6623e9d9c3a6a1eb2d3aeda8696210
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-18 09:25:14 +0700

    feat: add Dashbord state

--- lib/src/features/dashboard/logic/dashboard_bloc.dart
+++ lib/src/features/dashboard/logic/dashboard_bloc.dart
@@ -1,10 +1,9 @@
-
-
+import 'package:fitness_app/src/features/dashboard/logic/dasboard_state.dart';
 import 'package:fitness_app/src/features/dashboard/logic/navigation_bar_item.dart';
 import 'package:fitness_app/src/router/coordinator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class DashboardBloc extends Cubit<XNavigationBarItems> {
+class DashboardBloc extends Cubit<DashboardState> {
   /*
   /// Change to config router. It need to be corrected
   DashboardBloc(super.current) {
@@ -20,18 +19,15 @@ class DashboardBloc extends Cubit<XNavigationBarItems> {
     return false;
   }
 */
-  DashboardBloc(super.current);
+  DashboardBloc() : super(DashboardState.ds());
 
   void onDestinationSelected(int index) {
-    emit(XNavigationBarItems.values[index]);
-    AppCoordinator.goNamed(state.route.name);
+    emit(state.copyWith(item: XNavigationBarItems.values[index]));
+    AppCoordinator.goNamed(XNavigationBarItems.values[index].route.name);
   }
 
   void goHome() {
-    emit(XNavigationBarItems.home);
-    AppCoordinator.goNamed(state.route.name);
+   emit(state.copyWith(item: XNavigationBarItems.home));
+    AppCoordinator.goNamed(XNavigationBarItems.home.route.name);
   }
-}
-
-class RemoteConfigService {
 }
--- lib/src/features/dashboard/widget/bottom_navigation_bar.dart
+++ lib/src/features/dashboard/widget/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/src/features/dashboard/logic/dasboard_state.dart';
 import 'package:fitness_app/src/features/dashboard/logic/dashboard_bloc.dart';
 import 'package:fitness_app/src/features/dashboard/logic/navigation_bar_item.dart';
 import 'package:flutter/material.dart';
@@ -8,23 +9,30 @@ class XBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DashboardBloc, XNavigationBarItems>(
+    return BlocBuilder<DashboardBloc, DashboardState>(
+      buildWhen: (previous, current) {
+        return previous.item.selectedIndex != current.item.selectedIndex;
+      },
       builder: (context, state) {
         return NavigationBar(
-          selectedIndex: state.index,
+          selectedIndex: state.item.selectedIndex,
           onDestinationSelected:
               context.read<DashboardBloc>().onDestinationSelected,
-          destinations: XNavigationBarItems.values
-              .map(
-                (e) => NavigationDestination(
-                  label: e.label,
-                  icon: Icon(e.icon),
-                  selectedIcon: Icon(e.selectedIcon),
-                ),
-              )
-              .toList(),
+          destinations: _buildListItem(),
         );
       },
     );
+  }
+
+  List<Widget> _buildListItem() {
+    return XNavigationBarItems.values
+        .map(
+          (e) => NavigationDestination(
+            label: e.label,
+            icon: Icon(e.icon),
+            selectedIcon: Icon(e.selectedIcon),
+          ),
+        )
+        .toList();
   }
 }
