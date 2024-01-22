92d03c914cb74e4fa81231aa39bbc27a65f2ec0d
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-22 10:12:22 +0700

    fix: move SystemChrome to app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,16 +3,23 @@ import 'package:fitness_app/src/features/setting/logic/setting_bloc.dart';
 import 'package:fitness_app/src/features/setting/logic/setting_state.dart';
 import 'package:fitness_app/src/localization/localization_utils.dart';
 import 'package:fitness_app/src/router/router.dart';
+import 'package:fitness_app/src/themes/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
+import 'package:flutter/services.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: AppColors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
     final appRouter = GetIt.I<AppRouter>();
     return BlocProvider<SettingBloc>(
       create: (BuildContext context) {
--- lib/src/features/home/view/home_view.dart
+++ lib/src/features/home/view/home_view.dart
@@ -1,16 +1,11 @@
 import 'package:fitness_app/src/themes/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ));
     return const Scaffold(
       backgroundColor: AppColors.white,
       body: Center(
