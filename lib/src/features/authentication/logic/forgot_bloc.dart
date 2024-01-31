b1481963534d0e81d68f763c03e11cf0246ad8a0
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-01-31 14:28:11 +0700

    feat: add forgot password logic and password reset template

--- lib/src/features/authentication/logic/forgot_bloc.dart
+++ lib/src/features/authentication/logic/forgot_bloc.dart
@@ -1,5 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:fitness_app/src/features/authentication/model/email_fromz.dart';
+import 'package:fitness_app/src/localization/localization_utils.dart';
+import 'package:fitness_app/src/network/domain_manager.dart';
+import 'package:fitness_app/src/network/model/common/result.dart';
+import 'package:fitness_app/src/router/coordinator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:formz/formz.dart';
@@ -9,18 +13,46 @@ part 'forgot_state.dart';
 class ForgotBloc extends Cubit<ForgotState> {
   ForgotBloc() : super(const ForgotState());
 
-  Future onEnteredConfirmPassword(BuildContext context) async {
-    if (state.email.isPure) {
-      emit(state.copyWith(
-        email: const EmailFormzInput.dirty(''),
-      ));
+  DomainManager get domain => DomainManager();
+
+  Future forgotPassword(BuildContext context) async {
+    if (state.email.isValid == false || state.status.isInProgress) {
+      return;
     }
     emit(state.copyWith(
-      error: state.email.errorOf(context),
+      status: FormzSubmissionStatus.inProgress,
     ));
+
+    final MResult<String> result =
+        await domain.sign.forgotPassword(state.email.value);
+    if (result.isSuccess) {
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(S.of(context).check_email),
+          ),
+        );
+      }
+      AppCoordinator.pop();
+    } else {
+      emit(
+        state.copyWith(
+          status: FormzSubmissionStatus.failure,
+          error: result.error,
+        ),
+      );
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(S.of(context).failed),
+          ),
+        );
+      }
+    }
   }
 
   void onEmailChanged(String value) {
+    print(state.email.value);
     final email = state.email.isPure
         ? EmailFormzInput.pure(value)
         : EmailFormzInput.dirty(value);
