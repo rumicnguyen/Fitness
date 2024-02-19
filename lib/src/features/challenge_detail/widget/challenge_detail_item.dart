10d979b46daa17cf06365546c3f5126208fa7862
Dominic-go <154771765+Dominic-go@users.noreply.github.com>
2024-02-19 16:32:22 +0700

    Feature/Challenge detail (#31)
    * feat: add most popular logic
    
    * feat: add see all workout
    
    * feat: add set goal
    
    * feat: add challenge your stats
    
    * feat: add challenge detail
    
    * fix: resolve comment in #31
    
    * fix: remove XToast in challenge_detail_bloc.dart

--- lib/src/features/challenge_detail/widget/challenge_detail_item.dart
+++ lib/src/features/challenge_detail/widget/challenge_detail_item.dart
@@ -1,13 +1,11 @@
 import 'package:fitness_app/src/network/model/workout/workout.dart';
-import 'package:fitness_app/src/router/coordinator.dart';
-import 'package:fitness_app/src/themes/colors.dart';
 import 'package:fitness_app/src/themes/styles.dart';
 import 'package:fitness_app/src/utils/string_utils.dart';
 import 'package:fitness_app/widgets/image_widget.dart';
 import 'package:flutter/material.dart';
 
-class ViewItem extends StatelessWidget {
-  const ViewItem({
+class ChallengeDetailItem extends StatelessWidget {
+  const ChallengeDetailItem({
     super.key,
     required this.item,
   });
@@ -16,32 +14,19 @@ class ViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 5),
+    return SizedBox(
       width: double.infinity,
       height: 70,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          GestureDetector(
-            onTap: () {
-              AppCoordinator.showWorkoutDetailsScreen(id: item.id);
-            },
-            child: Container(
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: AppColors.lightGray,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ImageWidget(
-                image: item.thumbnail,
-                borderRadius: 10,
-              ),
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: ImageWidget(
+              image: item.thumbnail,
+              borderRadius: 10,
             ),
           ),
           const SizedBox(
