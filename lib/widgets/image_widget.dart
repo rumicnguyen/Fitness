398b6a7ca53785803f00dbd7c9b87206ff6268f5
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-02-19 15:19:59 +0700

    fix: resolve comment in #28

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,8 +1,7 @@
 import 'package:fitness_app/src/network/data/enum/storage/storage_folder.dart';
-import 'package:fitness_app/src/network/data/storage/firebase_storage_reference.dart';
 import 'package:flutter/material.dart';
 
-class ImageWidget extends StatefulWidget {
+class ImageWidget extends StatelessWidget {
   const ImageWidget({
     super.key,
     this.folder = StorageFolder.workouts,
@@ -23,36 +22,10 @@ class ImageWidget extends StatefulWidget {
   final double? borderRadius;
 
   @override
-  State<ImageWidget> createState() => _ImageWidgetState();
-}
-
-class _ImageWidgetState extends State<ImageWidget> {
-  String url = '';
-  Future syncData() async {
-    FirebaseStorageReference ref = FirebaseStorageReference();
-    final result = await ref.get(
-      folder: widget.folder,
-      data: widget.image,
-    );
-    if (result.isSuccess && result.data != null) {
-      setState(() {
-        url = result.data!;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    url = '';
-    syncData();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return widget.borderRadius != null
+    return borderRadius != null
         ? ClipRRect(
-            borderRadius: BorderRadius.circular(widget.borderRadius ?? 30),
+            borderRadius: BorderRadius.circular(borderRadius ?? 30),
             child: _buildBody(),
           )
         : _buildBody();
@@ -60,11 +33,11 @@ class _ImageWidgetState extends State<ImageWidget> {
 
   Widget _buildBody() {
     return Image.network(
-      url,
-      fit: widget.fit,
-      alignment: widget.alignment,
-      width: widget.width,
-      height: widget.height,
+      image,
+      fit: fit,
+      alignment: alignment,
+      width: width,
+      height: height,
     );
   }
 }
