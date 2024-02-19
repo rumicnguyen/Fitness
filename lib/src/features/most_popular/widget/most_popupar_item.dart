ce1d15b25d0252058c74e830beda482273ddb782
Dominic-go <dominic.nguyen.gos@gmail.com>
2024-02-19 14:15:54 +0700

    fix: trans stateful to stateless at most_popupar_item.dart

--- lib/src/features/most_popular/widget/most_popupar_item.dart
+++ lib/src/features/most_popular/widget/most_popupar_item.dart
@@ -1,51 +1,24 @@
-import 'package:fitness_app/src/network/data/enum/storage/storage_folder.dart';
-import 'package:fitness_app/src/network/data/storage/firebase_storage_reference.dart';
 import 'package:fitness_app/src/network/model/workout/workout.dart';
 import 'package:fitness_app/src/router/coordinator.dart';
 import 'package:fitness_app/src/themes/colors.dart';
 import 'package:fitness_app/src/themes/styles.dart';
 import 'package:flutter/material.dart';
 
-class MostPopularItem extends StatefulWidget {
+class MostPopularItem extends StatelessWidget {
   const MostPopularItem({
     super.key,
     required this.item,
+    required this.image,
   });
 
   final MWorkout item;
-
-  @override
-  State<MostPopularItem> createState() => _MostPopularItemState();
-}
-
-class _MostPopularItemState extends State<MostPopularItem> {
-  String image = '';
-
-  Future syncData() async {
-    FirebaseStorageReference ref = FirebaseStorageReference();
-    final result = await ref.get(
-      folder: StorageFolder.workouts,
-      data: widget.item.thumbnail,
-    );
-    if (result.isSuccess && result.data != null) {
-      setState(() {
-        image = result.data!;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    image = '';
-    syncData();
-  }
+  final String image;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        AppCoordinator.showWorkoutDetailsScreen(id: widget.item.id);
+        AppCoordinator.showWorkoutDetailsScreen(id: item.id);
       },
       child: Container(
         decoration: BoxDecoration(
@@ -59,7 +32,7 @@ class _MostPopularItemState extends State<MostPopularItem> {
         ),
         child: Center(
           child: Text(
-            widget.item.name,
+            item.name,
             style: AppStyles.mostB,
             textAlign: TextAlign.center,
           ),
